588239e0073efed36ad3c208c3d92d552e8735e4
Robyn Alex <robyn.alexander1111@gmail.com>
2022-04-07 02:56:31 -0500

    init commit

--- app.js
+++ app.js
@@ -2,3 +2,21 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+const plus = (num) => {
+    return (plusNum) => num + plusNum;
+};
+
+const plus15 = plus(10);
+
+console.log(plus15(10));
+
+const numbers = [8, 8, 5, 3, 2];
+
+console.log(plus15(...numbers));
+
+const plus18 = plus(18);
+
+console.log(plus18(18));
+
+console.log(plus18(...numbers));
